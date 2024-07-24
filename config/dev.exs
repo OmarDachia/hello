90311616accc66e9bf28a032eff62f039b278495
Umar Dachia <omardachia@gmail.com>
2024-07-24 15:49:40 +0100

    seting up my first elixir app

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :hello, Hello.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "u-b1989[]",
   hostname: "localhost",
   database: "hello_dev",
   stacktrace: true,
